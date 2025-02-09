6559e1e979812797d894df2d737e2b8e313170f3
ichang0301 <oeh900301@gmail.com>
2025-02-09 18:53:22 +0900

    added tests for cruise motorbike and family car

--- 02_creational_patterns/2-4_abstract_factory/2-4-1_vehicle_manufacturing/car_factory_test.go
+++ 02_creational_patterns/2-4_abstract_factory/2-4-1_vehicle_manufacturing/car_factory_test.go
@@ -2,7 +2,7 @@ package abstract_factory
 
 import "testing"
 
-func TestCarFactory(t *testing.T) {
+func TestLuxuryCar(t *testing.T) {
 	carF, err := BuildFactory(CarFactoryType)
 	if err != nil {
 		t.Fatal(err)
@@ -21,4 +21,25 @@ func TestCarFactory(t *testing.T) {
 		t.Fatal("Struct assertion has failed")
 	}
 	t.Logf("Car vehicle has %d doors\n", luxuryCar.NumDoors())
+}
+
+func TestFamilyCar(t *testing.T) {
+	carF, err := BuildFactory(CarFactoryType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	carVehicle, err := carF.Build(FamilyCarType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Car vehicle has %d wheels\n", carVehicle.NumWheels())
+	t.Logf("Car vehicle has %d seats\n", carVehicle.NumSeats())
+
+	familyCar, ok := carVehicle.(Car) // type assertion
+	if !ok {
+		t.Fatal("Struct assertion has failed")
+	}
+	t.Logf("Car vehicle has %d doors\n", familyCar.NumDoors())
 }
--- 02_creational_patterns/2-4_abstract_factory/2-4-1_vehicle_manufacturing/motorbike_factory_test.go
+++ 02_creational_patterns/2-4_abstract_factory/2-4-1_vehicle_manufacturing/motorbike_factory_test.go
@@ -2,7 +2,7 @@ package abstract_factory
 
 import "testing"
 
-func TestMotorbikeFactory(t *testing.T) {
+func TestSportMotorbike(t *testing.T) {
 	MotorbikeF, err := BuildFactory(MotorbikeFactoryType)
 	if err != nil {
 		t.Fatal(err)
@@ -22,4 +22,26 @@ func TestMotorbikeFactory(t *testing.T) {
 	}
 
 	t.Logf("Sport motorbike has type %d\n", sportBike.GetMotorbikeType())
+}
+
+func TestCruiseMotorbike(t *testing.T) {
+	MotorbikeF, err := BuildFactory(MotorbikeFactoryType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	motorbikeVehicle, err := MotorbikeF.Build(CruiseMotorbikeType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Motorbike vehicle has %d wheels\n", motorbikeVehicle.NumWheels())
+	t.Logf("Motorbike vehicle has %d seats\n", motorbikeVehicle.NumSeats())
+
+	cruiseBike, ok := motorbikeVehicle.(Motorbike) // type assertion
+	if !ok {
+		t.Fatal("Motorbike vehicle is not of type Motorbike")
+	}
+
+	t.Logf("Sport motorbike has type %d\n", cruiseBike.GetMotorbikeType())
 }
