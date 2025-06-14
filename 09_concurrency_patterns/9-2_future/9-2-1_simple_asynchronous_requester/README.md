d6dcfa203adbdd390664847990c317be4481e6a4
ichang0301 <oeh900301@gmail.com>
2025-06-14 23:31:27 +0900

    added primitive 'feature design pattern'

---
+++ 09_concurrency_patterns/9-2_future/9-2-1_simple_asynchronous_requester/README.md
@@ -0,0 +1,11 @@
+# 9-2-1_simple_asynchronous_requester
+
+## Acceptance criteria
+
+We don't have functional requirements for this task.
+Instead, we will have technical requirements for it.
+
+- Delegate the function execution to a different Goroutine
+- The function will return a string (maybe) or an error
+- The handlers must be already defined before executing the function
+- The design must be reusable
---
+++ 09_concurrency_patterns/9-2_future/9-2-1_simple_asynchronous_requester/future.go
@@ -0,0 +1,32 @@
+package future
+
+type ExecuteStringFunc func() (string, error)
+type SuccessFunc func(string)
+type FailFunc func(error)
+
+type MaybeString struct {
+	successFunc SuccessFunc
+	failFunc    FailFunc
+}
+
+func (s *MaybeString) Success(fn SuccessFunc) *MaybeString {
+	s.successFunc = fn
+	return s
+}
+
+func (s *MaybeString) Fail(fn FailFunc) *MaybeString {
+	s.failFunc = fn
+	return s
+}
+
+func (s *MaybeString) Execute(fn ExecuteStringFunc) *MaybeString {
+	go func(s *MaybeString) {
+		str, err := fn()
+		if err != nil {
+			s.failFunc(err)
+		} else {
+			s.successFunc(str)
+		}
+	}(s)
+	return s
+}
